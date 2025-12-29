db8474d3051ff0905dd9db2bb339d8c2d2808916
Francisco <github.com.visa669@passinbox.com>
2025-12-30 00:19:44 +0100

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+
+export default withNuxt(
+  // Your custom configs here
+  skipFormatting,
+  {
+    files: ['app/**/*.vue'],
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/block-order': [
+        'error',
+        {
+          order: ['template', 'script', 'style'],
+        },
+      ],
+    },
+  },
+);
