6110821b16b6969937f38348ee4430d68b7b74c1
Francisco <github.com.visa669@passinbox.com>
2025-12-30 13:18:08 +0100

    Background

---
+++ app/components/home/section-title.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2 :class="cn('text-xl font-bold md:text-2xl lg:text-3xl', $props.class)">
+    <slot />
+  </h2>
+</template>
+
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+import type { HTMLAttributes } from 'vue';
+
+defineProps<{
+  class?: HTMLAttributes['class'];
+}>();
+</script>
