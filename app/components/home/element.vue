70d144cf9ce5badc129b3180b8008e1d1203f114
Francisco <github.com.visa669@passinbox.com>
2025-12-30 19:40:11 +0100

    Second image

--- app/components/home/element.vue
+++ app/components/home/element.vue
@@ -24,9 +24,36 @@
         />
       </div>
     </div>
+    <div
+      class="flex w-full flex-col items-center bg-blue-50 p-6 pt-5 pb-10 pl-10 md:flex-row"
+    >
+      <div class="mb-6 flex justify-center md:mb-0 md:w-1/2">
+        <img
+          :src="one"
+          alt="Put your learning"
+          width="500px"
+          height="400px"
+          class="rounded-lg"
+        />
+      </div>
+      <div class="text-center md:w-1/2 md:text-left">
+        <h3 class="mb-2 text-lg font-semibold text-green-600">
+          Step by step lessons
+        </h3>
+        <h2 class="mb-4 text-5xl font-bold text-gray-800">
+          Put your learning <br />
+          into practice
+        </h2>
+        <p class="text-gray-600">
+          Apply your learning with real world projects and learn everything you
+          need to take your career to the next level.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import one from '@/assets/images/one.png';
 import two from '@/assets/images/two.png';
 </script>
