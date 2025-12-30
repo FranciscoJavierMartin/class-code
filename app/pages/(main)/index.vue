52a3f233f8045f3951af82b727fb52ac6643fc19
Francisco <github.com.visa669@passinbox.com>
2025-12-30 13:31:06 +0100

    Text in header

--- app/pages/(main)/index.vue
+++ app/pages/(main)/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <section class="space-y-6 pt-6 pb-8 md:pt-10 md:pb-12 lg:py-32">
+    <header class="space-y-6 pt-6 pb-8 md:pt-10 md:pb-12 lg:py-32">
       <div
         class="relative isolate container flex max-w-5xl flex-col items-center gap-4 text-center"
       >
@@ -16,8 +16,35 @@
             class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
           />
         </div>
-        <span> </span>
+        <span
+          class="bg-muted rounded-2xl border px-4 py-1.5 text-sm font-medium shadow-lg"
+        >
+          Hey, welcome
+        </span>
+        <h1 class="text-3xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
+          Learn by doing with <br /><span class="">Code Class</span>
+        </h1>
+        <p
+          class="text-muted-foreground max-w-2xl leading-normal sm:text-xl sm:leading-8"
+        >
+          “You don’t understand anything until you learn it more than one way.”
+        </p>
+        <div class="flex flex-wrap items-center justify-center gap-3">
+          <NuxtLink :class="cn(buttonVariants({ size: 'lg' }))">
+            Explore now
+          </NuxtLink>
+          <NuxtLink
+            :class="cn(buttonVariants({ size: 'lg', variant: 'outline' }))"
+          >
+            Become an instructor
+          </NuxtLink>
+        </div>
       </div>
-    </section>
+    </header>
   </div>
 </template>
+
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+</script>
