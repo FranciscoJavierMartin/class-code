74d74d7f819aafa8f9b530716c9d020259880a87
Francisco <github.com.visa669@passinbox.com>
2025-12-30 11:36:32 +0100

    Add shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,34 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/hints', '@nuxt/image'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+  ],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui',
+  },
+
+  colorMode: {
+    classSuffix: '',
+  },
 
   nitro: {
     preset: 'bun',
