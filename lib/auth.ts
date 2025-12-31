d4eb19713b0d1087d3a53b862859c0a2b35f1c7f
Francisco <github.com.visa669@passinbox.com>
2025-12-31 10:55:44 +0100

    Add user model

---
+++ lib/auth.ts
@@ -0,0 +1,29 @@
+import { betterAuth } from 'better-auth';
+import { PrismaClient } from '@/generated/prisma/client';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter });
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [],
+  user: {
+    modelName: 'User',
+  },
+  session: {
+    modelName: 'Session',
+  },
+  account: {
+    modelName: 'Account',
+  },
+  verification: {
+    modelName: 'Verification',
+  },
+});
