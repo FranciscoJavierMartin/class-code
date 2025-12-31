b66b4d19fed5343495c4f0782496be36fdb23e80
Francisco <github.com.visa669@passinbox.com>
2025-12-31 19:50:04 +0100

    Add course category relation

--- server/api/courses/index.get.ts
+++ server/api/courses/index.get.ts
@@ -1,7 +1,14 @@
 import { prisma } from '~~/lib/prisma';
 
 export default defineEventHandler(async (event) => {
-  const courses = await prisma.course.findMany();
+  const courses = await prisma.course.findMany({
+    include: {
+      modules: true,
+      testimonials: true,
+      category: true,
+      instructor: true,
+    },
+  });
 
   return {
     courses,
