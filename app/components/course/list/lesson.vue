d0f21fa1bf47910970801f46b5c9d3da75a14ede
Francisco <github.com.visa669@passinbox.com>
2026-01-04 20:36:29 +0100

    Split into components

---
+++ app/components/course/list/lesson.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="$props.class">
+    <button
+      :class="
+        cn(
+          'flex w-full items-center gap-x-2 text-sm font-medium text-slate-500 transition-all hover:text-slate-600',
+        )
+      "
+    >
+      <div class="flex items-center gap-x-2">
+        <Tv :size="16" class="text-slate-500" />
+        What is React?
+      </div>
+    </button>
+    <button
+      :class="
+        cn(
+          'flex w-full items-center gap-x-2 text-sm font-medium text-slate-500 transition-all hover:text-slate-600',
+        )
+      "
+    >
+      <div class="flex items-center gap-x-2">
+        <Tv :size="16" class="text-slate-500" />
+        What is React?
+      </div>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { Tv } from 'lucide-vue-next';
+import { cn } from '@/lib/utils';
+
+defineProps<{ class?: HTMLAttributes['class'] }>();
+</script>
---
+++ app/components/course/list/module.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <AccordionItem value="item-1">
+      <AccordionTrigger>Introduction</AccordionTrigger>
+      <AccordionContent>
+        <div
+          class="mt-4 mb-6 flex flex-wrap items-center gap-x-5 text-sm text-gray-600"
+        >
+          <span class="flex items-center gap-1.5">
+            <Video class="size-4" /> 12 lessons
+          </span>
+          <span class="flex items-center gap-1.5">
+            <NotepadText class="size-4" /> 10 notes
+          </span>
+          <span class="flex items-center gap-1.5">
+            <FileQuestion class="size-4" /> 10 quiz
+          </span>
+          <span class="flex items-center gap-1.5">
+            <Radio class="size-4" /> 1 Live class
+          </span>
+        </div>
+        <CourseListLesson class="space-y-3" />
+      </AccordionContent>
+    </AccordionItem>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Video, NotepadText, FileQuestion, Radio } from 'lucide-vue-next';
+</script>
