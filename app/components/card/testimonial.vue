4a22f45eb278b941cb65acee84bae79e36f20fef
Francisco <github.com.visa669@passinbox.com>
2026-01-04 19:39:52 +0100

    Testimonial card

---
+++ app/components/card/testimonial.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sm:break-inside-avoid">
+    <blockquote class="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
+      <div class="flex items-center gap-4">
+        <img
+          :alt="name"
+          :src="avatar"
+          width="56"
+          height="56"
+          className="size-14 rounded-full object-cover"
+        />
+        <div>
+          <p class="mt-0.5 text-lg font-medium text-gray-900">{{ name }}</p>
+          <div class="flex justify-center gap-0.5 text-yellow-600">
+            <Star
+              v-for="(_, index) in Array(integer)"
+              :key="index"
+              class="size-5 fill-yellow-600"
+            />
+            <StarHalf v-if="showHalfStar" class="size-5 fill-yellow-600" />
+          </div>
+        </div>
+      </div>
+      <p class="mt-4 text-gray-700">
+        {{ text }}
+      </p>
+    </blockquote>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Star, StarHalf } from 'lucide-vue-next';
+
+const { rating } = defineProps<{
+  avatar: string;
+  name: string;
+  rating: number;
+  text: string;
+}>();
+
+const integer = computed<number>(() => Math.floor(rating));
+const showHalfStar = computed<boolean>(() => rating % 1 >= 0.5);
+</script>
--- app/pages/(main)/courses/[id].vue
+++ app/pages/(main)/courses/[id].vue
@@ -304,10 +304,21 @@
           class="mx-auto w-full max-2xl:w-[90%]"
         >
           <CarouselContent>
-            <CarouselItem> 1 </CarouselItem>
-            <CarouselItem> 2 </CarouselItem>
-            <CarouselItem> 3 </CarouselItem>
-            <CarouselItem> 4 </CarouselItem>
+            <CarouselItem
+              v-for="(_, index) in new Array(5).fill('')"
+              :key="index"
+              class="md:basis-1/2 lg:basis-1/3"
+            >
+              <CardTestimonial
+                name="John Doe"
+                avatar="https://i.pravatar.cc/56"
+                :rating="4.5"
+                text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa sit
+        rerum incidunt, a consequuntur recusandae ab saepe illo est quia
+        obcaecati neque quibusdam eius accusamus error officiis atque voluptates
+        magnam!"
+              />
+            </CarouselItem>
           </CarouselContent>
           <CarouselPrevious />
           <CarouselNext />
